79e26b27b78fce6de6b258ce56aa4c353c3ae726
Eti Ijeoma <53005190+Aijeyomah@users.noreply.github.com>
2020-10-22 15:42:06 +0100

    fix start script

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 export default {
-  DATABASE_URL: process.env.TOTAL_LIMS_SYSTEM_POSTGRES_DEV_URL || process.env.DATABASE_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
   REDIS_URL: process.env.REDIS_URL,
   SEND_GRID_API: process.env.TOTAL_LIMS_SEND_GRID_API_KEY,
   TOTAL_LIMS_BASE_URL: "",
