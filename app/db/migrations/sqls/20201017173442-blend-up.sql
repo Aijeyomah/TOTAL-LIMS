563c57704611114c9609e492ae3eb6e941830a2f
Eti Ijeoma <53005190+Aijeyomah@users.noreply.github.com>
2020-10-22 21:10:42 +0100

    ft-auth

--- app/db/migrations/sqls/20201017173442-blend-up.sql
+++ app/db/migrations/sqls/20201017173442-blend-up.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS blend(
-    id SERIAL PRIMARY KEY,
-    product_id INT REFERENCES products(id),
-    test VARCHAR(100)[] NOT NULL,
-    method VARCHAR(100)[] NOT NULL,
-    specification VARCHAR(100)[]
-);
+-- CREATE TABLE IF NOT EXISTS blend(
+--     id SERIAL PRIMARY KEY,
+--     product_id INT REFERENCES products(id),
+--     test VARCHAR(100)[] NOT NULL,
+--     method VARCHAR(100)[] NOT NULL,
+--     specification VARCHAR(100)[]
+-- );
