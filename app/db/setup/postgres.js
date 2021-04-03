820a6708583f8730b267cff17eba7d6d1abebfa3
Eti Ijeoma <53005190+Aijeyomah@users.noreply.github.com>
2021-04-03 22:02:02 +0100

    ..

--- app/db/setup/postgres.js
+++ app/db/setup/postgres.js
@@ -2,17 +2,25 @@ import promise from 'bluebird';
 import pg from 'pg-promise';
 import config from '../../../config/env';
 
-const { DATABASE_URL } = config;
+const { DATABASE_URL, TOTAL_LIMS_SYSTEM_NODE_ENV } = config;
+
+
+let ssl = null;
+if (TOTAL_LIMS_SYSTEM_NODE_ENV === 'development') {
+    ssl = { rejectUnauthorized: false };
+}
 
 const options = {
     promiseLib: promise,
-    ssl: {
-        rejectUnauthorized: false
-    }
+};
+const dbConfig = {
+    connectionString: DATABASE_URL,
+    max: 30,
+    ssl: ssl
 };
 
 const pgp = pg(options);
 
-const db = pgp(DATABASE_URL);
+const db = pgp(dbConfig);
 
 export default db;
