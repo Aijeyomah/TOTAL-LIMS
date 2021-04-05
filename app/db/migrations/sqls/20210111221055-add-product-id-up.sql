15104f5b22641467f8e8ae6ded306e7d3042eb8f
Eti Ijeoma <53005190+Aijeyomah@users.noreply.github.com>
2021-04-05 10:43:29 +0100

    fix(pkg): add node version

--- app/db/migrations/sqls/20210111221055-add-product-id-up.sql
+++ app/db/migrations/sqls/20210111221055-add-product-id-up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE product_test_result
-	DROP COLUMN product_id;
+	DROP COLUMN IF EXISTS  product_id;
 
 ALTER TABLE product_result_details
-	ADD COLUMN product_id VARCHAR REFERENCES products(id);
+	ADD COLUMN IF NOT EXISTS product_id VARCHAR REFERENCES products(id);
